c09dcb286996445c701bbd3be63c769546436e36
Toby Allen <tallen@twilio.com>
2020-11-12 16:49:49 +1100

    Added Magic Links to email

--- functions/verify_bootstrap_confirm.js
+++ functions/verify_bootstrap_confirm.js
@@ -1,18 +1,96 @@
-exports.handler = function(context, event, callback) {
+// exports.handler = function(context, event, callback) {
 
-    const client = context.getTwilioClient();
+//     const client = context.getTwilioClient();
 
-	console.log(event);
+// 	console.log(event);
 	
-	client.verify.services(process.env.VERIFY_SERVICE)
-		.verificationChecks
-		.create ( {to: event.to, code: event.code})
-		.then(verification_check => {
-			console.log(verification_check.status);
-			callback(null, verification_check);
-		})
-		.catch( err => {
-			console.log(err);
-			callback(err);
+// 	client.verify.services(process.env.VERIFY_SERVICE)
+// 		.verificationChecks
+// 		.create ( {to: event.to, code: event.code})
+// 		.then(verification_check => {
+// 			console.log(verification_check.status);
+// 			callback(null, verification_check);
+// 		})
+// 		.catch( err => {
+// 			console.log(err);
+// 			callback(err);
+// 		});
+// };
+
+
+
+/**
+ *  Check Verification
+ *
+ *  This Function shows you how to check a verification token for Twilio Verify.
+ *
+ *  Pre-requisites
+ *  - Create a Verify Service (https://www.twilio.com/console/verify/services)
+ *  - Add VERIFY_SERVICE_SID from above to your Environment Variables (https://www.twilio.com/console/functions/configure)
+ *  - Enable ACCOUNT_SID and AUTH_TOKEN in your functions configuration (https://www.twilio.com/console/functions/configure)
+ *
+ *
+ *  Returns JSON:
+ *  {
+ *    "success": boolean,
+ *    "message": string
+ *  }
+ */
+
+exports.handler = function(context, event, callback) {
+	const response = new Twilio.Response();
+	response.appendHeader('Content-Type', 'application/json');
+	
+	// uncomment to support CORS
+	// response.appendHeader('Access-Control-Allow-Origin', '*');
+	// response.appendHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+	// response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
+	if (typeof event.to === 'undefined' ||
+		typeof event.code === 'undefined') {
+	  response.setBody({
+		"success": false,
+		"message": "Missing parameter."
+	  })
+	  response.setStatusCode(400);
+	  return callback(null, response);
+	}
+  
+	const client = context.getTwilioClient();
+	const service = context.VERIFY_SERVICE;
+	const to = event.to;
+	const code = event.code;
+  
+	client.verify.services(service)
+	  .verificationChecks
+	  .create({
+		to: to,
+		code: code
+	  })
+	  .then(check => {
+		if (check.status === "approved") {
+		  response.setStatusCode(200);
+		  response.setBody({
+			"success": true,
+			"message": "Verification success."
+		  });
+		  callback(null, response);
+		} else {
+		  response.setStatusCode(401);
+		  response.setBody({
+			"success": false,
+			"message": "Incorrect token."
+		  });
+		  callback(null, response);
+		}
+	  })
+	  .catch(error => {
+		console.log(error);
+		response.setStatusCode(error.status);
+		response.setBody({
+		  success: false,
+		  message: error.message
 		});
-};
+		callback(null, response);
+	  });
+  };
--- functions/verify_bootstrap_sendcode.js
+++ functions/verify_bootstrap_sendcode.js
@@ -1,17 +1,42 @@
 exports.handler = function(context, event, callback) {
     const client = context.getTwilioClient();
+
+    const protocol = (context.DOMAIN_NAME.startsWith('localhost:') ? 'http' : 'https')
+    const callback_url = `${protocol}://${context.DOMAIN_NAME}${context.PATH.substr(0, context.PATH.lastIndexOf('/'))}/verify.html`
     
     console.log(event);
     
-    client.verify.services(process.env.VERIFY_SERVICE)
-       .verifications
-       .create({    to: event.to,
-                    locale : event.locale,
-                    channel: event.channel
-               })
-       .then(verification => callback(null,verification))
-       .catch( err => {
-          console.log(err);
-          callback(err);
-     });
+    if (event.channel == 'email') {
+     client.verify.services(process.env.VERIFY_SERVICE)
+     .verifications
+     .create({    to: event.to,
+                  locale : event.locale,
+                  channel: event.channel,
+                  channelConfiguration: {
+                    substitutions: { // used in email template
+                      email: event.to,
+                      callback_url: callback_url
+                    }
+                  }
+             })
+     .then(verification => callback(null,verification))
+     .catch( err => {
+        console.log(err);
+        callback(err);
+   });
+    } else {
+     client.verify.services(process.env.VERIFY_SERVICE)
+     .verifications
+     .create({    to: event.to,
+                  locale : event.locale,
+                  channel: event.channel
+             })
+     .then(verification => callback(null,verification))
+     .catch( err => {
+        console.log(err);
+        callback(err);
+   });
+    }
+
+
   };
